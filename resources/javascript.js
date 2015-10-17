db2fadc0b1ca0bebf084eff3c2680f1b7c07d35e
MichelleDong <dong.michelle.r@gmail.com>
2015-10-17 14:57:19 -0700

    Moved JavaScript from HTML file to javascript.js file

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,8 @@
-// Your Javascript will go here!
+    <script type="text/javascript">
+      function replaceName() {
+        var myNewName = window.prompt('Pick a new name!');
+        document.getElementById('user-name').innerHTML = myNewName;
+      }
+      
+      document.onclick = replaceName;
+    </script>
